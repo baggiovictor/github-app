420aa1af70d2819fbfde6c5c997b08525eee54f6
victor.baggio <baggiobaggiovictor@gmail.com>
2023-03-09 20:42:07 -0300

    feat: create hook to consume github api

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { fetchData } from '../utils/fetchData';
+
+export const useApiData = (url: string) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      const apiData = await fetchData(url);
+      setData(apiData);
+    };
+
+    fetchDataFromApi();
+  }, [url]);
+
+  return data;
+};

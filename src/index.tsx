8dc9689a95a3ca9ce1d420e8bb153aba706c18eb
victor.baggio <baggiobaggiovictor@gmail.com>
2023-03-09 20:43:32 -0300

    feat: add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>OPA</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>po <Outlet /></div>}>
+          <Route path='' element={<div>{<Home />}</div>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

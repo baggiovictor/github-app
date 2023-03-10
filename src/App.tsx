f2b8cab947233b8842a2c5673c7e906fac9af385
victor.baggio <baggiobaggiovictor@gmail.com>
2023-03-09 23:57:26 -0300

    feat: add profileData

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/header/Header';
+import Profile from './pages/Profile/Profile';
+import Repository from './pages/Repository/Repository';
 
 
 
@@ -11,7 +13,14 @@ export function App() {
     return (
         <BrowserRouter>
             <Header />
+            <h1></h1>
+            <Routes>
+                <Route path='/' element={<Profile />} />
+                <Route path='/:username' element={<Profile />} />
+                <Route path='/:username/:reponame' element={<Repository />} />
+            </Routes>
 
+            {/* <Footer /> */}
             <GlobalStyles />
         </BrowserRouter>
     )
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Container, Main, LeftSide, RightSide } from './styles';
+import ProfileData from '../../components/ProfileData/ProfileData';
+
+
 
 const Profile: React.FC = () => {
-    return <div />;
+    return (
+        <Container>
+            <Main>
+                <LeftSide>
+                    <ProfileData
+                        username={'baggiovictor'}
+                        name={'Victor Baggio'}
+                        avatarUrl={'https://avatars.githubusercontent.com/u/65255655?v=4'}
+                        followers={200}
+                        following={10}
+                        company={'Totvs'}
+                        location={'Joinville, Brazil'}
+                        email={'baggiobaggiovictor@gmail.com'}
+                        blog={'linkedin.com/in/victorbaggio'}
+                    />
+                </LeftSide>
+                <RightSide></RightSide>
+            </Main>
+        </Container>
+    );
 }
 
 export default Profile;
